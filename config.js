fce27790b72735f7db56da7e6a6b4c626deabdf2
dohabinhthai <157684901+dohabinhthai@users.noreply.github.com>
2024-03-02 21:23:04 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Vy';
+const giftUrl = 'https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/349652407_569447165357209_4708902387456753901_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeEibfUnJyVqhr7KY3MJZdgCmWAfuOUddTOZYB-45R11M4Sqlb7I8oiNksvLCHkQgc_SQ8HyaFFoAhu6-7WaUqUc&_nc_ohc=NS8Q-rLZrWIAX-4tDUf&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfDnjMcWObOVIL92KIfQLbvW-OHPip9DiNb7lFy4QVSd_w&oe=65E7BED0scontent.fsgn2-3.fna.fbcdn.net/v/t39.30808-6/411954971_1430780404497645_524452383385474546_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeGessQFq_2kM7SAfpHTr1SqhhgJLIV5bOWGGAkshXls5VyY_5chej_nBYdV0idVD7_8x2HPviZAlIAVOwQYi0Ex&_nc_ohc=DnYImm0upzgAX8z_CAe&_nc_ht=scontent.fsgn2-3.fna&oh=00_AfDfA-Qkq2Qp_0te0MQPbcuCBmMFcEzJvpQO45t8h1meIQ&oe=65E729C4';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng người bạn tri kỉ';
+const contentCard = 'Chúc Vy tràn ngập niềm vui và những nụ cười.Mong điều đẹp nhất sẽ đến với Vy trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '349652407_569447165357209_4708902387456753901_n.jpg'
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
